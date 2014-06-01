8b1c99fc42e038e5bfc8b0f2b947bcd3831fe7f2
Sean <sean.ballinger@gmail.com>
2014-06-01 11:40:32 +0300

    display list of coordinates, adding CGPoints to NSMuA is not working

--- TouchTracker/TouchTrackerViewController.m
+++ TouchTracker/TouchTrackerViewController.m
@@ -12,17 +12,26 @@
 
 @interface TouchTrackerViewController ()
 //@property (nonatomic, strong) UIView *myView;
+@property (nonatomic, strong) NSMutableArray *touchList;
 @end
 
 @implementation TouchTrackerViewController
 
 @synthesize xDisplay = _xDisplay, yDisplay = _yDisplay;
+@synthesize touchList = _touchList;
+@synthesize textDisplay = _textDisplay;
 //@synthesize myView = _myView;
 
 //- (void)loadView
 //{
 //    [self setView:[[TouchDrawView alloc] initWithFrame:CGRectZero]];
 //}
+
+- (NSMutableArray *)touchList
+{
+    if (!_touchList) _touchList = [[NSMutableArray alloc] init];
+    return _touchList;
+}
 
 - (void)touchesBegan:(NSSet *)touches
            withEvent:(UIEvent *)event
@@ -33,6 +42,9 @@
         CGPoint location = [t locationInView:[[TouchDrawView alloc] initWithFrame:CGRectZero]];
         self.xDisplay.text = [NSString stringWithFormat:@"x: %g", location.x];
         self.yDisplay.text = [NSString stringWithFormat:@"y: %g", location.y];
+        //[self.touchList addObject:[NSString stringWithFormat:@"(%g,%g)", location.x, location.y]];
+        //[self.touchList addObject:(NSObject *) location;
+        self.textDisplay.text = [self.textDisplay.text stringByAppendingString:[NSString stringWithFormat:@"(%g,%g)", location.x, location.y]];
     }
 }
 
