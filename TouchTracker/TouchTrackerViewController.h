5fceb9e2e8f123fbcfdc8e580583d212b928fc41
Sean <sean.ballinger@gmail.com>
2014-06-01 11:40:32 +0300

    display list of coordinates, adding CGPoints to NSMuA is not working

--- TouchTracker/TouchTrackerViewController.h
+++ TouchTracker/TouchTrackerViewController.h
@@ -13,5 +13,6 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *xDisplay;
 @property (weak, nonatomic) IBOutlet UILabel *yDisplay;
+@property (weak, nonatomic) IBOutlet UITextView *textDisplay;
 
 @end
