07509703582453cd6485236a981a8bdef941c2fe
Sean <sean.ballinger@gmail.com>
2014-06-01 13:51:28 +0300

    added brain, made view loading prettier

---
+++ TouchTracker/LokeyBrain.h
@@ -0,0 +1,13 @@
+//
+//  LokeyBrain.h
+//  TouchTracker
+//
+//  Created by Sean on 6/1/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LokeyBrain : NSObject
+
+@end
