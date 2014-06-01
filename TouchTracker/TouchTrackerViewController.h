0109be6e3295990039110792219d5ceab38b0376
Sean <sean.ballinger@gmail.com>
2014-06-01 08:32:53 +0300

    Initial Commit

---
+++ TouchTracker/TouchTrackerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TouchTrackerViewController.h
+//  TouchTracker
+//
+//  Created by Sean on 6/1/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchTrackerViewController : UIViewController
+
+@end
---
+++ TouchTracker/TouchTrackerViewController.m
@@ -0,0 +1,29 @@
+//
+//  TouchTrackerViewController.m
+//  TouchTracker
+//
+//  Created by Sean on 6/1/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import "TouchTrackerViewController.h"
+
+@interface TouchTrackerViewController ()
+
+@end
+
+@implementation TouchTrackerViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
