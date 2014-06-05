65cc76b9a70090a4b4c48c92d876491719ee8484
Sean <sean.ballinger@gmail.com>
2014-06-05 22:43:10 +0300

    added Snake class to be filled later

---
+++ TouchTracker/Snake.h
@@ -0,0 +1,13 @@
+//
+//  Snake.h
+//  TouchTracker
+//
+//  Created by Sean on 6/5/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Snake : NSObject
+
+@end
---
+++ TouchTracker/Snake.m
@@ -0,0 +1,13 @@
+//
+//  Snake.m
+//  TouchTracker
+//
+//  Created by Sean on 6/5/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import "Snake.h"
+
+@implementation Snake
+
+@end
