84b4d0d7e3babb27878f575558c5259e31347c3e
Sean <sean.ballinger@gmail.com>
2014-06-11 01:17:59 -0400

    ran BBUncrustify

--- TouchTracker/DictionaryBuilder.m
+++ TouchTracker/DictionaryBuilder.m
@@ -9,7 +9,7 @@
 #import "DictionaryBuilder.h"
 #import "Snake.h"
 
-@interface DictionaryBuilder()
+@interface DictionaryBuilder ()
 
 @property (nonatomic, strong) NSArray *dictionaryWords;
 @property (nonatomic, strong) Snake *snake;
@@ -22,37 +22,32 @@
 @synthesize snake = _snake;
 @synthesize snakeDictionary = _snakeDictionary;
 
-- (NSArray *)dictionaryWords
-{
-    if (!_dictionaryWords)
-    {
-        _dictionaryWords = [[NSMutableArray alloc] init];
-        NSString *path = [[NSBundle mainBundle] pathForResource:@"dict" ofType:@"txt"];
-        NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
-        _dictionaryWords = [content componentsSeparatedByString:@"\r"];
-    }
-    
-    return _dictionaryWords;
+- (NSArray *)dictionaryWords {
+	if (!_dictionaryWords) {
+		_dictionaryWords = [[NSMutableArray alloc] init];
+		NSString *path = [[NSBundle mainBundle] pathForResource:@"dict" ofType:@"txt"];
+		NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+		_dictionaryWords = [content componentsSeparatedByString:@"\r"];
+	}
+
+	return _dictionaryWords;
 }
 
-- (void)writeSnakeDictionary
-{
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *dictPath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"snakeDictionary.plist"];
-    _snakeDictionary = [NSMutableDictionary dictionaryWithCapacity:[self.dictionaryWords count]];
-    for (NSString *word in self.dictionaryWords)
-    {
-        if ([word length] >= 3)
-        {
-            NSString *path = [self.snake snakePathOfWord:word];
-            NSMutableArray *list = [_snakeDictionary objectForKey:path];
-            if (list == nil) list = [[NSMutableArray alloc] init];
-            [list addObject:word];
-            [_snakeDictionary setObject:list forKey:path];
-        }
-    }
-    //[NSKeyedArchiver archiveRootObject:_snakeDictionary toFile:@"snakeDictionary.plist"];
-    [self.snakeDictionary writeToFile:dictPath atomically:YES];
+- (void)writeSnakeDictionary {
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *dictPath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"snakeDictionary.plist"];
+	_snakeDictionary = [NSMutableDictionary dictionaryWithCapacity:[self.dictionaryWords count]];
+	for (NSString *word in self.dictionaryWords) {
+		if ([word length] >= 3) {
+			NSString *path = [self.snake snakePathOfWord:word];
+			NSMutableArray *list = [_snakeDictionary objectForKey:path];
+			if (list == nil) list = [[NSMutableArray alloc] init];
+			[list addObject:word];
+			[_snakeDictionary setObject:list forKey:path];
+		}
+	}
+	//[NSKeyedArchiver archiveRootObject:_snakeDictionary toFile:@"snakeDictionary.plist"];
+	[self.snakeDictionary writeToFile:dictPath atomically:YES];
 }
 
 @end
