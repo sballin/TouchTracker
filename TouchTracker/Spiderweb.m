3420d712bd5ce9c42caf510030058816e31caa91
Sean <sean.ballinger@gmail.com>
2014-06-25 23:27:42 -0700

    added Spiderweb class

---
+++ TouchTracker/Spiderweb.m
@@ -0,0 +1,13 @@
+//
+//  Spiderweb.m
+//  TouchTracker
+//
+//  Created by Sean on 6/25/14.
+//  Copyright (c) 2014 Sean. All rights reserved.
+//
+
+#import "Spiderweb.h"
+
+@implementation Spiderweb
+
+@end
