12b686c78f125ca6765af7d65f22bf73d6d6df18
Sean <sean.ballinger@gmail.com>
2014-07-10 23:49:57 -0700

    added ideas to readme, refactored Fraction methods

--- README.md
+++ README.md
@@ -2,28 +2,61 @@
 
 A weird keyboard.
 
-## Status
 
-Absolute shit at guessing. Need more precise dictionary because xs aren't helping with lookup.
+## ☆ The x problem
 
-## Easy tasks
+x definition is messing up a lot of words -> Bash user input and compare with everything that matches! Will there be any reason to use xs then?
 
-- Make plist format dictionaries using Python
-- Use NSSets in snakeDictionary instead of NSMutableArrays to do intersection stuff and maybe speedup
+## ☆ Timer
 
-## Long-term tasks
+- Wait for all of input then calculate word score
+    - 0.5 second timer
+    - Show when it's up to see calculation time
 
-- Accuracy
+## ★ Brain 
+
+- Combines several analyses
+    - Snake path: +1 if unbashed path has best of anything
+    - Fraction path: take top N from each list
+    - UpDown: 
+    - Mountains: 
+
+## Neighbors
+
+- Letters right next to each other could give some information. 
+- Example: `l` pressed, short distance detected between strokes, then it's probably one of `m` `k` `o` `p`. 
+- Go through list of candidates and check that min distances correspond to adjacent letters. Boost likelihood score if so.
+
+## UpDown
+
+- Gaussian distribution?
+- NOT relative to last press if possible -> two modes
+- Do a least-squares fit of points suspected to be on same row, shuffle around points to get most parallel set of lines.
+- Trace a path through points suspected to be on the same row, and note its curviness.
+
+## Mountains
+
+- Gaussian distribution
+- Fit, scale
+    - Get endpoints, compress/expand comparisons, integrate difference
+
+## Optimizations
+
+- Can make plist format dictionaries using __Python__
+- Use __NSSet__ in snakeDictionary instead of NSMutableArray to do intersection stuff and maybe speedup
+- __Average__ fraction paths for words and keep in an __NSMutableDictionary__
+
+## THE FUTURE
+
 - Short words
 - Combined score
     - Touch data interpretation
     - Frequency
-- Pressing space
+- Pressing space, shift, punctuation
 
 ## Resources
 
 - [Gutenberg frequency lists](https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists#Project_Gutenberg)
 
-## Cleanup
+![](viz.png)
 
-- Get rid of snakePathOfWord method
--- TouchTracker/TouchTrackerBrain.m
+++ TouchTracker/TouchTrackerBrain.m
@@ -9,25 +9,13 @@
 #import "TouchTrackerBrain.h"
 #import "KeyMath.h"
 
-@interface TouchTrackerBrain ()
-@property (nonatomic, strong) KeyMath *keyboard;
-- (void)addToSequence:(CGPoint)touch;
-- (NSArray *)fractionPath:(NSMutableArray *)touchSequence;
-@end
-
 @implementation TouchTrackerBrain
 
 @synthesize touchSequence = _touchSequence;
-@synthesize keyboard = _keyboard;
 
 - (NSMutableArray *)touchSequence {
 	if (!_touchSequence) _touchSequence = [[NSMutableArray alloc] init];
 	return _touchSequence;
-}
-
-- (KeyMath *)keyboard {
-    if (!_keyboard) _keyboard = [[KeyMath alloc] init];
-    return _keyboard;
 }
 
 - (void)clearTouchSequence {
@@ -42,52 +30,6 @@
 	CGPoint touch;
 	[[self.touchSequence objectAtIndex:i] getValue:&touch];
 	return touch;
-}
-
-- (NSArray *)fractionPath:(NSMutableArray *)touchSequence {
-	float total = 0.0;
-	NSMutableArray *path = [[NSMutableArray alloc] init];
-	for (int i = 0; i < [touchSequence count] - 1; i++) {
-        CGPoint firstPoint, secondPoint;
-        [[touchSequence objectAtIndex:i] getValue:&firstPoint];
-        [[touchSequence objectAtIndex:i+1] getValue:&secondPoint];
-		float distance =  [KeyMath distanceBetween:firstPoint and:secondPoint];
-		[path addObject:[NSNumber numberWithFloat:distance]];
-		total += distance;
-	}
-	for (int i = 0; i < [path count]; i++) {
-		float fraction = [[path objectAtIndex:i] floatValue] / total;
-		[path replaceObjectAtIndex:i withObject:[NSNumber numberWithFloat:fraction]];
-	}
-	return [path copy];
-}
-
-- (float)errorForWord:(NSString *)word {
-	NSArray *touchPath = [self fractionPath:self.touchSequence];
-    NSMutableArray *wordSequence = [self.keyboard modelTouchSequenceFor:word];
-	NSArray *wordPath = [self fractionPath:wordSequence];
-	float totalError = 0.0;
-	for (int i = 0; i < [touchPath count]; i++) {
-		float touchFraction = [[touchPath objectAtIndex:i] floatValue];
-		float wordFraction = [[wordPath objectAtIndex:i] floatValue];
-        float increment = [KeyMath errorBetween:touchFraction and:wordFraction];
-		totalError += increment;
-	}
-	return totalError;
-}
-
-- (NSString *)bestMatchFor:(NSMutableArray *)words {
-	float leastError = INFINITY;
-	NSString *bestMatch;
-	for (NSString *word in words) {
-		float error = [self errorForWord:word];
-//        NSLog(@"%@, %f\n", word, error);
-		if (error < leastError) {
-			bestMatch = word;
-			leastError = error;
-		}
-	}
-	return [bestMatch stringByAppendingString:[NSString stringWithFormat:@" %f", leastError]];
 }
 
 @end
